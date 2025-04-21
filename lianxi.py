685acdb4e224c8a0a157b16563053654e3f6a74a
dailibin <1483413512@qq.com>
2025-04-21 17:58:06 +0800

    lianxi.py

---
+++ lianxi.py
@@ -0,0 +1,21 @@
+print("hello python")
+# hahaha
+"""
+jsdojla saohfkhaslk 
+"""
+# shohfo
+# sahdalsk
+# sdhkahlkj
+print("12315"
+      "")
+name = "小明"
+age = 12
+sex = "男"
+print(name, "性别", sex, "年龄", age, "岁")
+haha = input("请输入")
+print(haha)
+print(f"xingmingshi {name},年龄是{age},性别是{sex}")
+
+
+def login():
+    print("HelloKitty")
